b090b7c26e53c8ec399f63500dbbe78e5d008a70
Yero Rudzinskas <yeroslavas@gmail.com>
2015-09-22 16:19:48 -0400

    fuctional but incomplete p2 password gen

---
+++ README.md
@@ -0,0 +1,11 @@
+#P2 Yero Rudzinskas
+s
+##xkcd random password generator
+This project is a php based pseudo-random password generator working off of a dictionary with over 300,000 words. This allows for the hisghest level of security
+while allowing users to improve their vocabularies
+##this project can be viewed at
+p2.jurgislives.me
+##Screencast demo:
+<<youtube address here>>
+there are no credentials required and no reference to outside code was made, though StackOverflow, CodeCademy, and PHP.net made their value known. 
+
---
+++ logic.php
@@ -0,0 +1,67 @@
+<?php
+
+$words = array();
+
+// Open the file
+$fp = @fopen("word_list.txt", 'r'); 
+
+// Add each line to an array
+if ($fp) 
+{
+   $words = explode("\n", fread($fp, filesize('word_list.txt')));
+}
+//setting up some globals
+
+$z = count($words);
+//sanity check
+
+$how_many = preg_replace('/[^0-9]/', '', $_POST['request']);
+if(strlen($how_many) < 2) 
+{
+$how_many = ($_POST['request']);
+
+//establishing arrays for char, num, and the password
+$char = array("!", "@", "#", "$", "^", "&", "*", ":)", ":(");
+$num = array("0", "1", "2", "3", "4", "5", "6", "7", "8", "9");
+$key = array();
+
+//randomly select desired number of words from $words	
+for ($i = 0; $i < ($how_many); $i++)
+	{
+		$chooser = rand(0, $z);
+		array_push ($key, ($words[$chooser]));
+	}
+//tandomly select a character if desired
+if ($_POST["character"] == 'yes')
+{
+	
+	$x = rand(0, 8);
+	array_push($key, $char[$x]);
+	
+}
+
+//randomly select number if desired
+
+if ($_POST["number"] == 'yes')
+{
+	for($i = 0; $i < 2; $i++)
+	{
+		$y = rand(0,9);
+		array_push($key, $num[$y]);
+	}
+}
+//return the password	
+print "Your Password is:<br><br>";
+
+//echo camel-case password
+foreach($key as $item) 
+{
+    echo ucwords($item);
+};
+}
+else
+{
+	print "You may select a number up to 9";
+	return 500;
+}
+?>
